55eb8094fd804198925d06869bda61b01f78e9cb
Yann.Xia <me.yan.xia@qq.com>
2017-05-16 23:18:00 +0800

    Update host_is_exist.sh

--- host_is_exist.sh
+++ host_is_exist.sh
@@ -1,13 +1,13 @@
 # Usage:  ./host_is_exist.sh '172.16.[0-9]\{1,3\}\.[0-9]\{1,3\}'
 
-for loop in 1 2 3
+for loop in 1 2 3 4 5 6
 do
     if ifconfig | awk -F "[: ]+" '/inet addr:/ { if ($4 != "127.0.0.1") print $4 }' | grep -o $1 >/dev/null
     then
         echo "online"
         break
     else
-        echo "sleep 1m for ip alive...."
-        sleep 1m
+        echo "sleep 10s for ip alive...."
+        sleep 10s
     fi
 done
